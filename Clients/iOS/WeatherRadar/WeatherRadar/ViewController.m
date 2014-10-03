17cd054ce3db0e9518a30f8c1ba0058b25066c63
Karl Bloedorn <karlbloedorn@karls-mbp.lan>
2014-10-02 21:16:53 -0500

    initial commit new project for iOS added

---
+++ Clients/iOS/WeatherRadar/WeatherRadar/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WeatherRadar
+//
+//  Created by Karl Bloedorn on 10/2/14.
+//  Copyright (c) 2014 Karl Bloedorn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
