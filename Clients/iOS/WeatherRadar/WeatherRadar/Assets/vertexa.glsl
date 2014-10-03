444a3abde74091410046421369bb11a64512badb
Karl Bloedorn <karlbloedorn@karls-mbp.lan>
2014-10-02 23:06:53 -0500

    Added existing assets to iOS project

---
+++ Clients/iOS/WeatherRadar/WeatherRadar/Assets/vertexa.glsl
@@ -0,0 +1,11 @@
+attribute vec2 position;
+//attribute vec4 color;
+
+varying lowp vec4 colorVarying;
+uniform mat4 modelViewProjectionMatrix;
+
+void main()
+{
+    colorVarying = vec4(1.0,1.0,1.0,1.0); // color;
+    gl_Position = modelViewProjectionMatrix * vec4(position, 0.0, 1.0);
+}
